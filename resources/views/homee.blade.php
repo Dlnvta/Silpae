b70ae1d8c5935eb204cac234fa078c9bae9235c8
Dlnvta <delanovita800@gmail.com>
2021-02-15 11:02:14 +0700

    failed

---
+++ resources/views/homee.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Shuffle Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="homepage/assets/img/favicon.png" rel="icon">
+  <link href="homepage/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="homepage/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="homepage/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="homepage/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="homepage/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="homepage/assets/vendor/owl.carousel/homepage/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="homepage/assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="homepage/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Shuffle - v2.3.1
+  * Template URL: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="homepage/assets/img/slide/slide-4.jpg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Laundry Shire</span></h2>
+                <p class="animate__animated animate__fadeInUp">Bersih Harum dan Wangi | Komitmen Kami</p>
+                <a href="login" class="btn-get-started animate__animated animate__fadeInUp scrollto">Sign In</a>
+                <a href="register" class="btn-get-started animate__animated animate__fadeInUp scrollto">Sign Up</a>
+              </div>
+            </div>
+          </div>
+          
+          <!-- Vendor JS Files -->
+  <script src="homepage/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="homepage/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="homepage/assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="homepage/assets/vendor/php-email-form/validate.js"></script>
+  <script src="homepage/assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="homepage/assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="homepage/assets/vendor/counterup/counterup.min.js"></script>
+  <script src="homepage/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="homepage/assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="homepage/assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="homepage/assets/js/main.js"></script>
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laundray</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,10 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    DELA NOVITA
-                </div>
-                <div class="title m-b-md">
-                    XII RPL 2
+                    Laravel
                 </div>
 
                 <div class="links">
